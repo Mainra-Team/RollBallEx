e50645b47c8e7a59ca1bcda976c784ce4edf89a2
farismiqdad01 <56767913+farismiqdad01@users.noreply.github.com>
2022-11-23 06:13:21 +0700

    add trail renderer @ball

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -14,6 +14,8 @@ public class ChangeColor : MonoBehaviour
         if (other.gameObject.CompareTag("Box"))
         {
             rend.material.color = other.gameObject.GetComponent<Renderer>().material.color;
+            rend.material.SetColor("_EmissionColor", other.gameObject.GetComponent<Renderer>().material.color);
+            GetComponent<TrailRenderer>().material.color = other.gameObject.GetComponent<Renderer>().material.color;
         }
     }
 }
